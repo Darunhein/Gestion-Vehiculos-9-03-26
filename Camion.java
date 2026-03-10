b0f3537359e3ff2f626469d88434bc84ae08a48c
Darunhein <darayava2006@gmail.com>
2026-03-09 20:09:33 -0600

    Camion class started atributes done as well as construct

---
+++ Camion.java
@@ -0,0 +1,14 @@
+public class Camion extends Vehiculo {
+    
+    
+    protected int capacidadCarga;
+    protected int numeroEjes;
+
+    public Camion(String marca, String modelo, int anio, double precio, int capacidadCarga, int numeroEjes){
+        super(marca, modelo, anio, precio);
+        this.capacidadCarga = capacidadCarga;
+        this.numeroEjes = numeroEjes;
+    }
+
+    
+}
