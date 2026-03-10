d05a2e1ba8faf5d5763db96e051c4e4369f4feba
Darunhein <darayava2006@gmail.com>
2026-03-09 20:58:03 -0600

    Code now pretty yay

--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
 public class Main {
     public static void main (String[]args){
+
+        //Object of PedirDatos
         PedirDatos pd = new PedirDatos();
 
+        //Variable for the switch
         int opcion ;
 
+        //Start of the do while
         do {
 
+            //Menu with greeting
             System.out.println("Hello There");
             System.out.println("Menu (Please press the corresponding number to the option you desire)");
             opcion = pd.pedirEntero("1 Crear Auto \n2 Crear Camion \n3 Mostrar total de Vehiculos creados \n4 Para Salir", 1, 4);
 
+            //Start of the switch
             switch(opcion){
+
                 case 1:
                     System.out.println("Escribe los datos de tu Auto");
                     String marcaA = pd.pedirCadena("Marca del Auto");
@@ -55,6 +62,8 @@ public class Main {
                     System.out.println("GudBye");
                 break;
             }
+
+        //End of the do while    
         }while(opcion != 4); 
     }
 }
