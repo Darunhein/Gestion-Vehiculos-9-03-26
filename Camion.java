e4fd595c89f77b2b46258d79859cd723a1f67ce9
Darunhein <darayava2006@gmail.com>
2026-03-09 20:15:15 -0600

    Camion class complete starting compilation

--- Camion.java
+++ Camion.java
@@ -1,14 +1,22 @@
 public class Camion extends Vehiculo {
     
-    
+    //Atributes
     protected int capacidadCarga;
     protected int numeroEjes;
 
+    //Construct
     public Camion(String marca, String modelo, int anio, double precio, int capacidadCarga, int numeroEjes){
         super(marca, modelo, anio, precio);
         this.capacidadCarga = capacidadCarga;
         this.numeroEjes = numeroEjes;
     }
-
     
+    //Methods
+    public void cargarMercancia(){
+        System.out.println("El camion " + getMarca() + "está cargando mercancía");
+    }
+
+    public void mostarCapacidad(){
+        System.out.println("El limite de la capacidad de carga del camion " + getMarca() + " es " + capacidadCarga);
+    }
 }
